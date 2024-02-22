f6765357becd3f341b47daf608cc895bd4c52969
andyhero <andyhero@node0.andyhero-190666.cascadefuse-pg0.wisc.cloudlab.us>
2024-02-22 10:53:50 -0600

    added init script for depend

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install build-essential
+sudo apt install autoconf libtool
+sudo apt-get install libibverbs-dev
+sudo apt-get install librdmacm-dev
+sudo apt-get install libnuma-dev
+sudo apt-get install libssl-dev
+sudo apt-get install openmpi-bin libopenmpi-dev
+sudo apt install libcrypto++-dev
+
